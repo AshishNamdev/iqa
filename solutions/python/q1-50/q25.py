bcbfbc384db6da0255b23ff80bb414f64ed3c871
Ashish Namdev <ashish28.sirt@gmail.com>
2019-11-17 13:01:39 +0530

    Added Q14 to Q25 question, answers and solution
    Includes: #22 #23 #24 #25 #26 #27 #28
    
    Signed-off-by: Ashish Namdev <ashish28.sirt@gmail.com>

---
+++ solutions/python/q1-50/q25.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 17, 2019
+
+@author: Ashish Namdev
+'''
+VERSION = '1.0.0'
+DATE = '2019-11-17'
+UPDATED = '2019-11-17'
+
+n = 10
+a = [i for i in range(1, n)]
+avg = sum(a)/n
+print("Average of elements in the %s = %s" % (a, avg))
+
